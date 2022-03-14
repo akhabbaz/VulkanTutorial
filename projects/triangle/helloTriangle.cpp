ec0a722624d404dfabd697a4003befa239272c54
Anton Khabbaz <akhabbaz@tutanota.com>
2022-03-14 16:39:33 -0400

    first Vulkan Instance working

--- projects/triangle/helloTriangle.cpp
+++ projects/triangle/helloTriangle.cpp
@@ -25,7 +25,8 @@ private:
 	     }
 	     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-	     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);	
+	     window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
     }
     void createInstance(){
 	    VkApplicationInfo appInfo{};
@@ -33,8 +34,23 @@ private:
 	    appInfo.pApplicationName = "Hello Triangle";
 	    appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
 	    appInfo.pEngineName = "No Engine";
-	    appInfo.engineVresion = VK_MAKE_VERSION(1, 0, 0);
+	    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
 	    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	    VkInstanceCreateInfo createInfo{};
+	    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	    createInfo.pApplicationInfo = &appInfo;
+	    uint32_t glfwExtensionCount = 0;
+	    const char** glfwExtensions;
+
+	    glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	    createInfo.enabledExtensionCount = glfwExtensionCount;
+	    createInfo.ppEnabledExtensionNames = glfwExtensions;
+	    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
+		    	throw std::runtime_error("failed to create instance!");
+	    }
+
     }
     void mainLoop() {
 		while (!glfwWindowShouldClose(window)) {
